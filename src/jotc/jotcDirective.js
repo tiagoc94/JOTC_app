edb77b2a1079311c6ab5784dc43c1fb8451de3e4
Tiago Conceição <tiago.conceicao@multivision.pt>
2023-03-04 23:48:48 +0000

    Creates a directive for the jotc game.

---
+++ src/jotc/jotcDirective.js
@@ -0,0 +1,52 @@
+(function() {
+    let User;
+    /**
+     */
+    class Jotc {
+        /**
+         * @param {$rootScope} scope
+         */
+        constructor(scope) {
+            this.scope = scope;
+            this.output = '';
+        }
+
+        onSubmit() {
+            console.log('submit');
+        }
+    }
+
+    angular.module("JOTC").directive('jotc', [
+        '$injector',
+        function($injector) {
+            User = $injector.get('User');
+            return {
+                restrict: 'E',
+                template: '<div>' +
+                    '  <div>Please enter the JOTC inputs.</div>' +
+
+                    '  <div>N must be an integer equal or greater than 2 and lesser or equal than 100.</div>' +
+                    '  <label><b>Number of clouds (N)</b></label>' +
+                    '  <input class="jotc-input" type="number" ><br>' +
+
+                    '  <div>The clouds must be N space-separated binary integers (0 or 1) where the first and last integers must be 0.' +
+                    '  0 denotes a safe cloud and 1 denotes a cloud that must be avoided.</div>' +
+                    '  <label><b>Clouds (N binary integers describing clouds)</b></label>' +
+                    '  <input class="jotc-input" type="number" ><br>\n' +
+
+                    '  <button class="fancy-button" ng-click="jotc.onSubmit()">Submit</button>' +
+
+                    '  <label><b>Output:</b></label>' +
+                    '  <div>{{jotc.output}}</div>' +
+                    '  <div>',
+                scope: {
+                    user: '=',
+                    adminMode: '='
+                },
+                link: (scope) => {
+                    scope.jotc = new Jotc(scope);
+                }
+            };
+        }
+    ]);
+})();

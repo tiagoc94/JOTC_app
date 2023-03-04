f8cd4867e0bbc2119f4b61efd2693a4f28ec394f
Tiago Conceição <tiago.conceicao@multivision.pt>
2023-03-04 23:51:57 +0000

    Creates a user class.

---
+++ src/classes/user.js
@@ -0,0 +1,24 @@
+(function() {
+    /**
+     */
+    class User {
+        /**
+         * @param {string} firstName
+         * @param {string} lastName
+         * @param {string} email
+         * @param {string} dateOfBirth
+         */
+        constructor(firstName, lastName, email, dateOfBirth) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.dateOfBirth = dateOfBirth;
+        }
+
+    }
+
+    angular.module("JOTC").factory('User', function() {
+            return User;
+        }
+    );
+})();

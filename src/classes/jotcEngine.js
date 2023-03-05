72b3d9a376979e8a0e9a6fd362543a07c5397fe6
Tiago Conceição <tiago.conceicao@multivision.pt>
2023-03-05 02:00:48 +0000

    Small changes

--- src/classes/jotcEngine.js
+++ src/classes/jotcEngine.js
@@ -7,46 +7,54 @@
 
         constructor(numberOfCloudsInput, cloudValuesInput) {
             this.numberOfCloudsInput = Number(numberOfCloudsInput);
-            this.cloudValues = cloudValuesInput.split(' ').map((value) => Number(value));
+            this.cloudValues = (cloudValuesInput == null || cloudValuesInput === '') ?
+                [] :
+                cloudValuesInput.split(' ').map((value) => value !== '' && Number(value));
         }
 
         /**
          * Checks if numberOfCloudsInput is a number between 2 and 100
          * @return {boolean}
+         * @private
          */
-        checkCloudNumber() {
+        _checkCloudNumber() {
             return !isNaN(this.numberOfCloudsInput) && 2 <= this.numberOfCloudsInput && this.numberOfCloudsInput <= 100;
         }
 
         /**
          * Checks if the values are only 1s and 0s
          * @return {boolean}
+         * @private
          */
-        checkCloudValues() {
-            return !this.cloudValues.some((value) => value !== CUMULUS && value !== THUNDERHEAD);
+        _checkCloudValues() {
+            return this.cloudValues.length > 0 &&
+                !this.cloudValues.some((value) => value !== CUMULUS && value !== THUNDERHEAD);
         }
 
         /**
          * Checks if the first and last values are 0
          * @return {boolean}
+         * @private
          */
-        checkFirstAndLastClouds() {
+        _checkFirstAndLastClouds() {
             return this.cloudValues[0] === CUMULUS && this.cloudValues[this.cloudValues.length - 1] === CUMULUS;
         }
 
         /**
          * Checks if the number input and values size match
          * @return {boolean}
+         * @private
          */
-        checkInputsMatch() {
+        _checkInputsMatch() {
             return this.cloudValues.length === this.numberOfCloudsInput;
         }
 
         /**
          * Checks if the game is winnable. The game is winnable if there arent more than two 1s in a row.
          * @return {boolean}
+         * @private
          */
-        checkGameWinnable() {
+        _checkGameWinnable() {
             let thunderheadsInARow = 0;
             const limit = 2;
 
@@ -70,19 +78,19 @@
          * @return {string|boolean}
          */
         validateInputs() {
-            if (!this.checkCloudNumber()) {
-                return 'The number of clouds must be an integer between 2 and 100.'
+            if (!this._checkCloudNumber()) {
+                return 'The number of clouds must be an integer between 2 and 100.';
             }
-            if (!this.checkCloudValues()) {
-                return 'The cloud values must be space-separated integers with values 1 or 0.'
+            if (!this._checkCloudValues()) {
+                return 'The cloud values must be space-separated integers with values 1 or 0.';
             }
-            if (!this.checkFirstAndLastClouds()) {
-                return 'The first and last clouds must be 0s.'
+            if (!this._checkFirstAndLastClouds()) {
+                return 'The first and last clouds must be 0s.';
             }
-            if (!this.checkInputsMatch()) {
-                return 'The number of clouds and the number of values must match.'
+            if (!this._checkInputsMatch()) {
+                return 'The number of clouds and the number of values must match.';
             }
-            if (!this.checkGameWinnable()) {
+            if (!this._checkGameWinnable()) {
                 return "There can't be more than one 1 in a row.";
             }
 

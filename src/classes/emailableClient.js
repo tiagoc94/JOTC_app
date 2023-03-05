cf5188c0f47b05dc255e5043d9b8082987e27bb3
Tiago Conceição <tiago.conceicao@multivision.pt>
2023-03-05 14:07:37 +0000

    Adds doc.

--- src/classes/emailableClient.js
+++ src/classes/emailableClient.js
@@ -6,6 +6,10 @@
      */
     class EmailableClient {
 
+        /**
+         * @param {string} email
+         * @return {Promise}
+         */
         verify(email) {
             const params = {
                 email: email,

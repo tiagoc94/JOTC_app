ea5b7ba38ad97f05aab9ded12f927d9c967e4ed6
Tiago Conceição <tiago.conceicao@multivision.pt>
2023-03-11 00:56:20 +0000

    Implements filtering.

--- src/adminInterface/adminInterfaceDirective.js
+++ src/adminInterface/adminInterfaceDirective.js
@@ -9,52 +9,70 @@
         constructor(scope) {
             this.scope = scope;
             this.onResetFilters();
-
-            this.jotcRequests = [
-                {
-                    name: 'userName',
-                    email: 'email@test',
-                    date: '2022-11-11',
-                    numberOfClouds: '4',
-                    cloudValues: '0 0 1 0',
-                    solution: '2'
-                },
-                {
-                    name: 'userName',
-                    email: 'email@test',
-                    date: '2022-11-14',
-                    numberOfClouds: '2',
-                    cloudValues: '0 0 1 0 0 0 0 0 0 0 0',
-                    solution: 'invalid message test'
-                },
-            ];
-
-            let i = 0;
-            while (i < 100) {
-                this.jotcRequests.push({
-                    name: 'userName',
-                    email: 'email@test',
-                    date: '2022-11-14',
-                    numberOfClouds: '2',
-                    cloudValues: '0 0 1 0 0 0 0 0 0 0 0',
-                    solution: 'invalid message test'
-                });
-
-                i++;
-            }
-
+            this.jotcRequests = [];
         }
 
+        /**
+         */
         onResetFilters() {
             this.emailQuery = '';
             this.dateFilterStart = null;
             this.dateFilterEnd = null;
         }
 
+        /**
+         */
         onSearch() {
-
+            let storedRequests = JOTCStorage.getAll();
+            storedRequests = this._filterEmail(storedRequests);
+            storedRequests = this._filterDateStart(storedRequests);
+            this.jotcRequests = this._filterDateEnd(storedRequests);
         }
 
+        /**
+         * @param {Object[]} jotcRequests
+         * @return {Object[]}
+         * @private
+         */
+        _filterEmail(jotcRequests) {
+            if (this.emailQuery === '') {
+                return jotcRequests;
+            }
+
+            return jotcRequests.filter((data) => data.email.includes(this.emailQuery));
+        }
+
+        /**
+         * @param {Object[]} jotcRequests
+         * @return {Object[]}
+         * @private
+         */
+        _filterDateStart(jotcRequests) {
+            if (this.dateFilterStart == null) {
+                return jotcRequests;
+            }
+
+            return jotcRequests.filter((data) => {
+                const requestDate = new Date(data.date);
+                return requestDate >= this.dateFilterStart;
+            });
+        }
+
+        /**
+         * @param {Object[]} jotcRequests
+         * @return {Object[]}
+         * @private
+         */
+        _filterDateEnd(jotcRequests) {
+            if (this.dateFilterEnd == null) {
+                return jotcRequests;
+            }
+
+            return jotcRequests.filter((data) => {
+                const requestDate = new Date(data.date);
+                return requestDate <= this.dateFilterEnd;
+            });
+        }
     }
 
     angular.module("JOTC").directive('adminInterface', [
@@ -90,14 +108,14 @@
                     '      <th class="table-cell">Date</th>' +
                     '      <th class="table-cell">Number of clouds (N)</th>' +
                     '      <th class="table-cell">Clouds</th>' +
-                    '      <th class="table-cell">Solution</th>' +
+                    '      <th class="table-cell">Output</th>' +
                     '      <tr ng-repeat="requestData in adminInterface.jotcRequests">' +
-                    '        <td class="table-cell">{{ requestData.name }}</td>' +
+                    '        <td class="table-cell">{{ requestData.userName }}</td>' +
                     '        <td class="table-cell">{{ requestData.email }}</td>' +
                     '        <td class="table-cell">{{ requestData.date }}</td>' +
                     '        <td class="table-cell">{{ requestData.numberOfClouds }}</td>' +
                     '        <td class="table-cell">{{ requestData.cloudValues }}</td>' +
-                    '        <td class="table-cell">{{ requestData.solution }}</td>' +
+                    '        <td class="table-cell">{{ requestData.output }}</td>' +
                     '      </tr>' +
                     '    </table>' +
 

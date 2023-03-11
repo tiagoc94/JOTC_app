2e49cfb65569e785fef5ca99ab0d6b5ee2f9a41a
Tiago Conceição <tiago.conceicao@multivision.pt>
2023-03-11 16:01:52 +0000

    Adds more tests for the login directive.

--- tests/loginDirective.spec.js
+++ tests/loginDirective.spec.js
@@ -3,8 +3,12 @@ describe('Login directive', function() {
     let $rootScope;
     let $httpBackend;
     let loginDirectiveElement;
+    let loginDirective;
     let $scope;
     let $compile;
+    let EmailVerifier;
+    let $q;
+    let User;
 
     const directiveHtml = `<login user="user"
                                   admin-mode="adminMode"
@@ -17,6 +21,9 @@ describe('Login directive', function() {
             $rootScope = $injector.get('$rootScope');
             $httpBackend = $injector.get('$httpBackend');
             $compile = $injector.get('$compile');
+            EmailVerifier = $injector.get('EmailVerifier');
+            $q = $injector.get('$q');
+            User = $injector.get('User');
         });
     });
 
@@ -26,15 +33,118 @@ describe('Login directive', function() {
         $scope.user = null;
         $scope.adminMode = false;
         $scope.loggedIn = false;
-        loginDirectiveElement = $compile(directiveHtml)($scope)[0].firstElementChild;
-        // const loginDirective = loginDirectiveElement.isolateScope().login;
+        loginDirectiveElement = $compile(directiveHtml)($scope);
+        loginDirective = loginDirectiveElement.isolateScope().login;
+        loginDirectiveElement = loginDirectiveElement[0].firstElementChild;
         $scope.$digest();
     });
 
-    it('initializes contains the expected elements', function() {
+    it('contains the expected elements', function() {
         expect(loginDirectiveElement.getElementsByTagName('input').length).toBe(4);
         expect(loginDirectiveElement.getElementsByClassName('invalid-login').length).toBe(0);
         expect(loginDirectiveElement.getElementsByTagName('button').length).toBe(1);
     });
 
+    it('validates empty fields', function() {
+        loginDirective.onLoginClick();
+        $scope.$digest();
+
+        expect(loginDirectiveElement.getElementsByClassName('invalid-login').length).toBe(3);
+        expect($scope.loggedIn).toBeFalse();
+    });
+
+    it('validates birth date', function() {
+        loginDirective.email = 'testEmail';
+        loginDirective.firstName = 'testFirstName';
+        loginDirective.lastName = 'testLastName';
+        loginDirective.dateOfBirth = new Date('2020-01-01')
+        $scope.$digest();
+        loginDirective.onLoginClick();
+        $scope.$digest();
+
+        const invalidElements = loginDirectiveElement.getElementsByClassName('invalid-login');
+        expect(invalidElements.length).toBe(1);
+        expect(invalidElements[0].type).toBe('date');
+
+        const invalidBirthDateFeedbackElem = loginDirectiveElement.getElementsByClassName('feedback-invalid')[1];
+        expect(invalidBirthDateFeedbackElem.classList).not.toContain('ng-hide');
+
+        expect($scope.loggedIn).toBeFalse();
+    });
+
+    it('shows user feedback when the email is invalid', function() {
+        spyOn(EmailVerifier, 'isEmailValid').and.callFake(function() {
+            const defer = $q.defer();
+            defer.reject();
+            return defer.promise;
+        });
+
+        loginDirective.email = 'testEmail';
+        loginDirective.firstName = 'testFirstName';
+        loginDirective.lastName = 'testLastName';
+        loginDirective.dateOfBirth = new Date('1994-01-01')
+        $scope.$digest();
+        loginDirective.onLoginClick();
+        $scope.$digest();
+
+        const invalidElements = loginDirectiveElement.getElementsByClassName('invalid-login');
+        expect(invalidElements.length).toBe(1);
+
+        const invalidEmailFeedbackElem = loginDirectiveElement.getElementsByClassName('feedback-invalid')[0];
+        expect(invalidEmailFeedbackElem.classList).not.toContain('ng-hide');
+
+        expect($scope.loggedIn).toBeFalse();
+    });
+
+    it('logs in successfully as non authenticated user', function() {
+        spyOn(EmailVerifier, 'isEmailValid').and.callFake(function() {
+            const defer = $q.defer();
+            defer.resolve();
+            return defer.promise;
+        });
+        spyOn(EmailVerifier, 'isEmailAdmin').and.callFake(function() {
+            const defer = $q.defer();
+            defer.reject();
+            return defer.promise;
+        });
+
+        loginDirective.email = 'testEmail';
+        loginDirective.firstName = 'testFirstName';
+        loginDirective.lastName = 'testLastName';
+        loginDirective.dateOfBirth = new Date('1994-01-01')
+        $scope.$digest();
+        loginDirective.onLoginClick();
+        $scope.$digest();
+
+        expect($scope.user).toBeDefined();
+        expect($scope.user).toBeInstanceOf(User);
+        expect($scope.adminMode).toBeFalse();
+        expect($scope.loggedIn).toBeTrue();
+    });
+
+    it('logs in successfully as authenticated user', function() {
+        spyOn(EmailVerifier, 'isEmailValid').and.callFake(function() {
+            const defer = $q.defer();
+            defer.resolve();
+            return defer.promise;
+        });
+        spyOn(EmailVerifier, 'isEmailAdmin').and.callFake(function() {
+            const defer = $q.defer();
+            defer.resolve();
+            return defer.promise;
+        });
+
+        loginDirective.email = 'testEmail';
+        loginDirective.firstName = 'testFirstName';
+        loginDirective.lastName = 'testLastName';
+        loginDirective.dateOfBirth = new Date('1994-01-01')
+        $scope.$digest();
+        loginDirective.onLoginClick();
+        $scope.$digest();
+
+        expect($scope.user).toBeDefined();
+        expect($scope.user).toBeInstanceOf(User);
+        expect($scope.adminMode).toBeTrue();
+        expect($scope.loggedIn).toBeTrue();
+    });
 });

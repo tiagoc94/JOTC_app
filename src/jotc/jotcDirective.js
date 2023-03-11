98b2cfac87d55f3f18606e7cc5a1717f25727665
Tiago Conceição <tiago.conceicao@multivision.pt>
2023-03-11 17:58:38 +0000

    Moves the html template to the directive.

--- src/jotc/jotcDirective.js
+++ src/jotc/jotcDirective.js
@@ -79,7 +79,29 @@
             JOTCStorage = $injector.get('JOTCStorage');
             return {
                 restrict: 'E',
-                templateUrl: '/src/jotc/jotc.tpl.html',
+                // Placing the html template directly in here instead of its own file using the "templateUrl" property
+                // to avoid wasting time figuring out how to the directive can access the html in the testing environment.
+                template: `<div>
+                            <div><b>Please enter the JOTC inputs.</b></div><br>
+                        
+                            <div>N must be an integer equal or greater than 2 and lesser or equal than 100.</div>
+                            <label><b>Number of clouds (N)</b></label>
+                            <input class="jotc-input" type="number" ng-model="jotc.numberOfCloudsInput"><br>
+                        
+                            <div>The clouds must be N space-separated binary integers (0 or 1) where the first and last integers must be 0.
+                                0 denotes a safe cloud and 1 denotes a cloud that must be avoided.</div>
+                            <label><b>Clouds (N binary integers describing clouds)</b></label>
+                            <input class="jotc-input" type="text" ng-model="jotc.cloudValueInput"><br>
+                        
+                            <div class="invalid" ng-show="!jotc.valid">{{jotc.invalidFeedback}}</div>
+                        
+                            <button class="fancy-button" ng-click="jotc.onSolve()">Solve</button><br>
+                        
+                            <div class="jotc-output">
+                                <label><b>Solution:</b></label>
+                                <div>{{jotc.solution}}</div>
+                            </div>
+                        </div>`,
                 scope: {
                     user: '='
                 },
